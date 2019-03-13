0a3f9f2727839bd84db629ef42fb0bedbe6c4e05
Felix Dollack <felix.dollack@googlemail.com>
2019-03-13 12:49:45 +0900

    Add forward pass function
    This function evaluates all nodes and computes the output of the network.

--- MiniFlow.py
+++ MiniFlow.py
@@ -141,3 +141,20 @@ def topological_sort(feed_dict):
             if len(G[m]['in']) == 0:
                 S.add(m)
     return L
+
+def forward_pass(output_node, sorted_nodes):
+    """
+    Performs a forward pass through a list of sorted nodes.
+
+    Arguments:
+
+        `output_node`: A node in the graph, should be the output node (have no outgoing edges).
+        `sorted_nodes`: A topologically sorted list of nodes.
+
+    Returns the output Node's value
+    """
+
+    for n in sorted_nodes:
+        n.forward()
+
+    return output_node.value
