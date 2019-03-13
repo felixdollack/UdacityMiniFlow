10b9c84ebb5dea8c8dca44dad720b4d714cb7720
Felix Dollack <felix.dollack@googlemail.com>
2019-03-13 12:42:00 +0900

    Add sigmoid node
    Implemented the sigmoid function.

--- MiniFlow.py
+++ MiniFlow.py
@@ -55,6 +55,23 @@ class Linear(Node):
         b = self.inbound_nodes[2].value
         self.value = np.dot(X, W) + b
 
+class Sigmoid(Node):
+    def __init__(self, node):
+        Node.__init__(self, [node])
+
+    def _sigmoid(self, x):
+        """
+        This method is separate from `forward` because it
+        will be used with `backward` as well.
+
+        `x`: A numpy array-like object.
+        """
+        return 1. / (1. + np.exp(-x)) # the `.` ensures that `1` is a float
+
+    def forward(self):
+        input_value = self.inbound_nodes[0].value
+        self.value = self._sigmoid(input_value)
+
 def topological_sort(feed_dict):
     """
     Sort generic nodes in topological order using Kahn's Algorithm.
