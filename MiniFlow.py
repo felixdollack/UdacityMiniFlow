094591e24eda44864aead3d16b1ae913fbbe446b
Felix Dollack <felix.dollack@googlemail.com>
2019-03-13 12:48:35 +0900

    Add MSE node
    Added a node calculating the mean squared error.

--- MiniFlow.py
+++ MiniFlow.py
@@ -72,6 +72,35 @@ class Sigmoid(Node):
         input_value = self.inbound_nodes[0].value
         self.value = self._sigmoid(input_value)
 
+class MSE(Node):
+    def __init__(self, y, a):
+        """
+        The mean squared error cost function.
+        Should be used as the last node for a network.
+        """
+        # Call the base class' constructor.
+        Node.__init__(self, [y, a])
+
+    def forward(self):
+        """
+        Calculates the mean squared error.
+        """
+        # NOTE: We reshape these to avoid possible matrix/vector broadcast
+        # errors.
+        #
+        # For example, if we subtract an array of shape (3,) from an array of shape
+        # (3,1) we get an array of shape(3,3) as the result when we want
+        # an array of shape (3,1) instead.
+        #
+        # Making both arrays (3,1) insures the result is (3,1) and does
+        # an elementwise subtraction as expected.
+        y = self.inbound_nodes[0].value.reshape(-1, 1)
+        a = self.inbound_nodes[1].value.reshape(-1, 1)
+
+        if (self.value == None):
+            self.value = 0
+        self.value += np.mean(np.square(y-a))
+
 def topological_sort(feed_dict):
     """
     Sort generic nodes in topological order using Kahn's Algorithm.
