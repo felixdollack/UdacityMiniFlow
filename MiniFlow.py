04287a6802077c578f804bbb6fb19c57b7536145
Felix Dollack <felix.dollack@googlemail.com>
2019-03-13 09:45:50 +0900

    Created generic node class
    This node is connected to other nodes (input/output) and keeps there reference.

---
+++ MiniFlow.py
@@ -0,0 +1,12 @@
+
+class Node(object):
+    def __init__(self):
+        # Node(s) from which this Node receives values
+        self.inbound_nodes = inbound_nodes
+        # Node(s) to which this Node passes values
+        self.outbound_nodes = []
+        # For each inbound_node, add the current Node as an outbound_node.
+        for n in self.inbound_nodes:
+            n.outbound_nodes.append(self)
+        # A calculated value
+        self.value = None
